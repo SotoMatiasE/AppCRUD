60faa2921ba385d1764e1d36b7211fc3fa12541b
Matias <matias_r12@hotmail.com>
2024-05-10 13:04:09 -0300

    first commit

--- app/src/main/java/com/example/appcrud/MainActivity.kt
+++ app/src/main/java/com/example/appcrud/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.appcrud
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.Menu
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.appcrud.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        //ingreso menu2
+        val btnNext: Button = findViewById(R.id.btnNext)
+        btnNext.setOnClickListener {
+            val intent: Intent = Intent(this, MenuIngreso::class.java)
+            startActivity(intent)
         }
     }
+
 }
---
+++ app/src/main/java/com/example/appcrud/MenuIngreso.kt
@@ -0,0 +1,16 @@
+package com.example.appcrud
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MenuIngreso : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu_ingreso)
+
+    }
+}
